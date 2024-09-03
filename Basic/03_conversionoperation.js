22c57cbd23817f6ea797d339cdfce9dbcee99387
Anish giri <anishgiri6393@gmail.com>
2024-09-03 14:14:16 +0000

    conversion & datatype

---
+++ Basic/03_conversionoperation.js
@@ -0,0 +1,37 @@
+// let score= 33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber= Number(score)
+
+// console.log(typeof score);
+// console.log(valueNumber);
+
+//  // "33"=33
+//  // "33abc"=NaN
+//  // true => 1, false=>0
+// let LoggedIn=1
+// let Booleanloggedln= Boolean(LoggedIn) 
+// console.log(Booleanloggedln);
+
+// let str1 = "anish "
+// let str2= "giri"
+// let str3= str1+ str2
+// console.log(str3);
+
+// equal and coparison greater than wegarh alag snytax hai 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+ 
+// plzz avoid this type conversion & comparison. this is confused code.
+
+
+
+
+
+
---
+++ Basic/04_datatypessummary.js
@@ -0,0 +1,39 @@
+// primitive  data type
+// 7 type= string, number boolean, null, undefiend , symbol, bigInt
+
+// reference ( NON - primitive)
+ 
+// array , object, function
+// float wegarh ka system sab const me a jata hai
+ 
+// const bigNumber= 1234567889n
+
+// const id= Symbol('123')
+
+// let myobj={
+//     name : "anish",
+//     age:  19,
+
+// }
+// const myfunction= function(){
+//     console.log("hello world");
+//  }
+// console.log(typeof bigNumber);
+//  const heros= ["saktiman", "naagraj",]
+//  console.log(typeof heros);
+
+// stack (primitive ) heap ( Non primitive)
+
+let myyoutubechannel = "anishgirineeraj"
+let anothername=  myyoutubechannel
+anothername = "chaiwithcode"
+
+console.log(anothername);
+console.log(myyoutubechannel);
+
+let user = {
+    email : "anisgiri.com",
+    upi: "dineshgiri8765"
+
+
+}
